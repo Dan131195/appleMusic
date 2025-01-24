e1f9b4caa89671fad59f4def6e9b9ad7293ce0ad
Dan131195 <daniele.franceschini1395@gmail.com>
2025-01-24 10:57:19 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import MyNav from "./components/MyNav";
 
 function App() {
-  return (
-    <>
-      <MyNav />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
